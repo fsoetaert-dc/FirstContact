5eab0cb5f7b58c2e338e8f8e48717000d9a3bb11
kilfour <kilfour@gmail.com>
2026-09-17 08:28:24 +0200

    code-review

--- FirstContact.Tests/UpdateContactTests.cs
+++ FirstContact.Tests/UpdateContactTests.cs
@@ -10,13 +10,15 @@ public class UpdateContactTests
         var repo = Substitute.For<IContactRepository>();
         var service = new ContactService(repo);
 
-        repo.GetById(20).Returns(new Contact("Filthy Frank"){Id = 20});
-        var request = new UpdateContactRequest {Name = "Dirty Dave"};
+        repo.GetById(20).Returns(new Contact("Filthy Frank") { Id = 20 });
+        var request = new UpdateContactRequest { Name = "Dirty Dave" };
         var response = service.UpdateContact(20, request);
 
         repo.Received(1).Commit();
 
         Assert.True(response);
+        // MME: Missing assertion on contact.Name changed to Dirty Dave
+        // put new Contact(... in a variable and check here
     }
 
     [Fact]
@@ -26,7 +28,7 @@ public class UpdateContactTests
         var service = new ContactService(repo);
 
         repo.GetById(20).Returns((Contact?)null);
-        var request = new UpdateContactRequest {Name = "Dirty Dave"};
+        var request = new UpdateContactRequest { Name = "Dirty Dave" };
         var response = service.UpdateContact(20, request);
 
         repo.Received(0).Commit();
